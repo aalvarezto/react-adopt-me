43427a19e8b372d666204d8dfedf9a34b9b858db
Andres Alvarez <ndralvz@gmail.com>
2020-10-25 01:39:31 -0300

    small fixes on FP utils :zap:

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,15 +2,15 @@
 
 const lowerNoSpaces = x => x.replace(" ", "").toLowerCase()
 const capFirstOf = x => x.slice(0, 1).toUpperCase() + x.slice(1)
+
 const map = x => y => y.map(x)
 const prop = x => y => y[x]
 const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x)
 const chain = compose(map, prop)
 const nullish = x => (x === undefined || x === null ? [] : x)
-
-export { lowerNoSpaces, capFirstOf, map, prop, compose, chain, peek, nullish }
-
-function peek(x) {
+const peek = x => {
 	console.log(x)
 	return x
 }
+
+export { lowerNoSpaces, capFirstOf, map, prop, compose, chain, peek, nullish }
